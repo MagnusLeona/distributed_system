5ba0a27d83871ae4db1f5a9e7c05acdf176b4e46
magnusleona <magnusleona@outlook.com>
2022-07-04 20:11:47 +0800

    chenht3

--- spring_source/src/main/java/magnus/leona/spring/ContainerTest.java
+++ spring_source/src/main/java/magnus/leona/spring/ContainerTest.java
@@ -5,8 +5,21 @@ import magnus.leona.spring.bean.Dog;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
 public class ContainerTest {
     public static void main(String[] args) {
+
+
+        List<Integer> integers = Arrays.asList(1, 2, 3, 4, 5, 6);
+        Optional<Integer> reduce = integers.stream().map(item -> item).reduce(Integer::sum);
+        Integer integer = reduce.get();
+        System.out.println("----------------------");
+        System.out.println(integer);
+
 
         System.out.println("&FactoryBean".substring(1));
         AnnotationConfigApplicationContext annotationConfigApplicationContext = new AnnotationConfigApplicationContext();
@@ -26,4 +39,5 @@ public class ContainerTest {
         classPathXmlApplicationContext.setConfigLocation("classpath:bean.xml");
         classPathXmlApplicationContext.refresh();
     }
+
 }
---
+++ spring_source/src/main/java/magnus/leona/spring/ContainerTestTest.java
@@ -0,0 +1,8 @@
+package magnus.leona.spring;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ContainerTestTest {
+
+
+}
